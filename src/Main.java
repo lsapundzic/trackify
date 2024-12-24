3c463086a1dd991d4aab145a4e7b2196fb6ab2bc
Luka <30961251+lsapundzic@users.noreply.github.com>
2024-12-24 16:22:21 +0100

    Added operating instructions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println("\t- - - PROGRAM STARTED - - -");
+
+        instructions();
+
+        System.out.println("\t- - - PROGRAM ENDED - - -");
+    }
+
+    public static void instructions() {
+        System.out.println("Instructions: ");
     }
 }
